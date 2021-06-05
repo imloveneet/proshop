9cdcdf8c00297ef498607a9a254f74c7d6a75908
Loveneet Kumar <max.due7@gmail.com>
2021-06-05 22:36:53 +0530

    user authentications

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 
@@ -10,11 +11,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('API is running...')
+  res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -23,8 +27,8 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-    PORT,
-    console.log(
-        `server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
-    )
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 )
