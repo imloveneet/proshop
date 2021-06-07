dd990135cb538edb38a7a0680f2eda4891d54dce
Loveneet Kumar <max.due7@gmail.com>
2021-06-07 20:45:19 +0530

    order create with paypal payment

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+const ShippingScreen = () => {
+  const cart = useSelector(state => state.cart)
+
+  const { shippingAddress } = cart
+
+  const [address, setAddress] = useState(
+    shippingAddress ? shippingAddress.address : ''
+  )
+  const [city, setCity] = useState(shippingAddress ? shippingAddress.city : '')
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress ? shippingAddress.postalCode : ''
+  )
+  const [country, setCountry] = useState(
+    shippingAddress ? shippingAddress.country : ''
+  )
+
+  const dispatch = useDispatch()
+
+  const history = useHistory()
+
+  const submitHandler = e => {
+    e.preventDefault()
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    history.push('/payment')
+  }
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 />
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='address'>
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter Address'
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='city'>
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter City'
+            value={city}
+            onChange={e => setCity(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='postalCode'>
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter Postal code'
+            value={postalCode}
+            onChange={e => setPostalCode(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='country'>
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter Country'
+            value={country}
+            onChange={e => setCountry(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default ShippingScreen
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,19 +2,33 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { cartReducers } from './reducers/cartReducers'
-
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer
+} from './reducers/orderReducers'
 import {
   productDetailsReducer,
   productListReducer
 } from './reducers/productReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducers,
   userLogin: userLoginReducer,
-  userRegister: userRegisterReducer
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -25,8 +39,20 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : null
+
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod')
+  ? JSON.parse(localStorage.getItem('paymentMethod'))
+  : null
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage
+  },
   userLogin: {
     userInfo: userInfoFromStorage
   }
