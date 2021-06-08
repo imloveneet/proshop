2fa265f783025f61e62c896bc9a593ba18b5aa35
Loveneet Kumar <max.due7@gmail.com>
2021-06-08 23:32:44 +0530

    user list on admin dashboard

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,7 +16,8 @@ import {
   userLoginReducer,
   userRegisterReducer,
   userDetailsReducer,
-  userUpdateProfileReducer
+  userUpdateProfileReducer,
+  userListReducer
 } from './reducers/userReducers'
 
 const reducer = combineReducers({
@@ -27,10 +28,11 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
-  orderListMy: orderListMyReducer
+  orderListMy: orderListMyReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
